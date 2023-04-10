bcb388b16fa6d321f51aec378cd5285080de9de8
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-04-10 21:51:01 +0530

    Create Practice makes us perfect.cpp

---
+++ Practice makes us perfect.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int ans=0;
+    int arr[4];
+    
+    for(int i=0;i<4;i++)
+    {
+        cin>>arr[i];
+    if (arr[i]>=10){
+        ans++;
+    }}
+    cout<<ans;
+    
+	
+	
+	return 0;
+}
